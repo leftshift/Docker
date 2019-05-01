26ed87bdc3d1b8ca261b10ae9120e31e092798fe
Adrian Schneider <adrian.schneider@abusix.com>
2019-05-01 19:45:24 +0200

    properly link dist, ensure directories exist, start all services

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -e
+set -e -x
 
 build_client() {
     echo "Cloning and building client..."
-    cd /opt/anfora/src/client
+    cd /opt/anfora/src/
+    git clone https://github.com/anforaProject/client.git client
+    cd client/
     yarn install
     yarn build
 }
@@ -16,24 +18,38 @@ sync_db() {
 
 generate_config() {
     echo "Generating config from yaml..."
-    cd /opt/anfora/src/config
-    pipenv run python commands.py -s --config docker-compose.yaml
+    cd /opt/anfora/src/
+    pipenv run python commands.py -s --config config/docker-compose.yaml
 }
 
-FIRST_RUN_SENTINEL=/var/anfora/initialized
+link_dist() {
+    echo "Movin client dist to /var/anfora and linking it back"
+    rm -rf /var/anfora/dist
+    mv /opt/anfora/src/client/dist /var/anfora/
+    ln -sf /var/anfora/dist /opt/anfora/src/client/dist
+}
+
+create_dirs() {
+    mkdir -p /var/anfora/media/max_resolution
+    mkdir -p /var/anfora/media/small
+}
+
+generate_config
+
+FIRST_RUN_SENTINEL=/opt/anfora/initialized
 if [ -e "$FIRST_RUN_SENTINEL" ]
 then
     echo "Not the first run of anfora, skipping setup..."
 else
     echo "Initial run, doing some setup..."
     build_client
+    link_dist
+    create_dirs
     sync_db
-    touch FIRST_RUN_SENTINEL
+    touch "$FIRST_RUN_SENTINEL"
 fi
-
-generate_config
 
 echo "Staring anfora components"
 cd /opt/anfora/src
-pipenv run huey_consumer.py tasks.main.huey -m 1 -w 4
+pipenv run huey_consumer.py tasks.main.huey -m 1 -w 4 &
 pipenv run python main.py
